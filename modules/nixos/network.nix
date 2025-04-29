b9aaf807536b351950f558d30f9e5f08c25cd8e7
evolve <elivanoa5@gmail.com>
2025-04-29 21:47:04 +0200

    no nameservers on wsl

--- modules/nixos/network.nix
+++ modules/nixos/network.nix
@@ -1,11 +1,17 @@
 {
   hostname,
   personal,
+  lib,
+  wsl,
   ...
-}: {
+}:
+{
   networking = {
     networkmanager.enable = true;
-    nameservers = ["1.1.1.1" "1.0.0.1"];
+    nameservers = lib.optionals wsl [
+      "1.1.1.1"
+      "1.0.0.1"
+    ];
     hostName = hostname;
   };
 
