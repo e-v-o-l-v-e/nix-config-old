e5648e6946f7fc6561e32b1ce82937422c08d81e
evolve <elivanoa5@gmail.com>
2025-05-03 13:36:02 +0200

    git default branch = main

--- modules/HM/git.nix
+++ modules/HM/git.nix
@@ -1,10 +1,12 @@
-{username, ...}: {
+{ username, ... }:
+{
   programs.git = {
     enable = true;
     userName = username;
     userEmail = "elivanoa5@gmail.com";
     extraConfig = {
       push.autoSetupRemote = true;
+      init.defaultBranch = "main";
     };
   };
 }
