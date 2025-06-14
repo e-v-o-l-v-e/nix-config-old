5cb96f3023f7b9df45fb2fd85e75ab8fda2ecc1a
evolve <elivanoa5@gmail.com>
2025-06-14 11:20:54 +0200

    install NH

--- home/env.nix
+++ home/env.nix
@@ -2,6 +2,7 @@
 {
   home.sessionVariables = {
     EDITOR = "nvim";
+    NH_FLAKE = "/home/evolve/nix-config";
     TERM = if (config.programs.kitty.enable) then "xterm-kitty" else "xterm-256color";
   };
 }
--- system/nix.nix
+++ system/nix.nix
@@ -2,7 +2,7 @@
 {
   # Cachix, Optimization settings and garbage collection automation
   nix = {
-    nixPath = [ "nixpkgs=${inputs.nixpkgs}" ] ;
+    nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
 
     settings = {
       auto-optimise-store = true;
@@ -14,10 +14,17 @@
       trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
     };
 
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 14d";
-    };
+    # gc = {
+    #   automatic = true;
+    #   dates = "weekly";
+    #   options = "--delete-older-than 14d";
+    # };
   };
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 14d";
+    flake = "/home/evolve/nix-config";
+  };
+
 }
