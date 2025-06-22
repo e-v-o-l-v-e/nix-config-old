1967fa677ba09c0d31793efa777cc45698b8a7ff
evolve <elivanoa5@gmail.com>
2025-06-22 17:31:40 +0200

    add waylander's key

--- system/sops.nix
+++ system/sops.nix
@@ -1,30 +1,25 @@
-{ inputs, username, config, lib, ... }:
-let
-  cfg = config.soft.sops-nix;
-in
+{ inputs, username, ... }:
 {
   imports = [
     inputs.sops-nix.nixosModules.sops
   ];
 
-  # config = lib.mkIf cfg.enable {
-    sops = {
-      defaultSopsFile = ../secrets/common.yaml;
-      validateSopsFiles = false;
+  sops = {
+    defaultSopsFile = ../secrets/common.yaml;
+    validateSopsFiles = false;
 
-      age = {
-        sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-        keyFile = "/var/lib/sops-nix/key.txt";
-        generateKey = true;
-      };
-
-      secrets = {
-      };
+    age = {
+      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+      keyFile = "/var/lib/sops-nix/key.txt";
+      generateKey = true;
     };
 
-    users.users.${username}.openssh.authorizedKeys.keys = [
-      (builtins.readFile ../secrets/public_keys/github.pub)
-      (builtins.readFile ../secrets/public_keys/git_unistra.pub)
-    ];
-  # };
+    secrets = {
+    };
+  };
+
+  users.users.${username}.openssh.authorizedKeys.keys = [
+    (builtins.readFile ../secrets/public_keys/github.pub)
+    (builtins.readFile ../secrets/public_keys/git_unistra.pub)
+  ];
 }
