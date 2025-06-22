b2b86ef10dfc900c17dfe8ce4c9320dc165afc9e
evolve <elivanoa5@gmail.com>
2025-06-22 18:11:39 +0200

    fix ssh hostname

--- home/shell/ssh.nix
+++ home/shell/ssh.nix
@@ -7,23 +7,14 @@
   programs.ssh = {
     enable = true;
     addKeysToAgent = "yes";
-    # extraConfig = ''
-    #   Host git.unistra.fr
-    #     AddKeysToAgent yes
-    #     IdentityFile ~/.ssh/keys/git_unistra
-    #
-    #   Host github.com
-    #     AddKeysToAgent yes
-    #     IdentityFile ~/.ssh/keys/github
-    # '';
 
     matchBlocks = {
       "github" = {
-        hostname = "github.com";
+        host = "github.com";
         identityFile = "~/.ssh/keys/github";
       };
-      "unistra" = {
-        hostname = "git.unistra.fr";
+      "iut" = {
+        host = "git.unistra.fr";
         identityFile = "~/.ssh/keys/git_unistra";
       };
     };
