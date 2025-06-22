504717685074adef08be05a9b629397f8b96a897
evolve <elivanoa5@gmail.com>
2025-06-22 17:51:11 +0200

    ssh matchbloc

--- home/shell/ssh.nix
+++ home/shell/ssh.nix
@@ -7,14 +7,25 @@
   programs.ssh = {
     enable = true;
     addKeysToAgent = "yes";
-    extraConfig = ''
-      Host git.unistra.fr
-        AddKeysToAgent yes
-        IdentityFile ~/.ssh/keys/git_unistra
+    # extraConfig = ''
+    #   Host git.unistra.fr
+    #     AddKeysToAgent yes
+    #     IdentityFile ~/.ssh/keys/git_unistra
+    #
+    #   Host github.com
+    #     AddKeysToAgent yes
+    #     IdentityFile ~/.ssh/keys/github
+    # '';
 
-      Host github.com
-        AddKeysToAgent yes
-        IdentityFile ~/.ssh/keys/github
-    '';
+    matchBlocks = {
+      "github" = {
+        hostname = "github.com";
+        identityFile = "~/.ssh/keys/github";
+      };
+      "unistra" = {
+        hostname = "git.unistra.fr";
+        identityFile = "~/.ssh/keys/git_unistra";
+      };
+    };
   };
 }
