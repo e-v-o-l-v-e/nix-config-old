d2336489cb9a530d8458565b4215eeeb52fdc089
evolve <elivanoa5@gmail.com>
2025-08-11 21:16:51 +0200

    use nix shell for pay-respect

--- home/shell/pay-respects.nix
+++ home/shell/pay-respects.nix
@@ -3,7 +3,15 @@
   programs.pay-respects = {
     enable = true;
     enableFishIntegration = true;
+
   };
+
+  home.file.".config//pay-respects/config.toml".text = ''
+    timeout = 3000
+    [package_manager]
+    package_manager = "nix"
+    install_method = "Shell"
+  '';
 
   programs.nix-index = {
     enable = true;
