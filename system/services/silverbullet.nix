9ee952cea6fd1db0f09c3ed3544f3ac634c74baa
evolve <elivanoa5@gmail.com>
2025-08-20 20:10:00 +0200

    fix silverbullet and update to 10.0.4

--- system/services/silverbullet.nix
+++ system/services/silverbullet.nix
@@ -1,19 +1,38 @@
-{ lib, username, config, ... }: 
-let
+{
+  lib,
+  pkgs,
+  username,
+  config,
+  ...
+}: let
   cfg = config.server;
 
   listenPort = 3333;
   spaceDir = cfg.ssdPath + "/silverbullet/space";
   fqdn = cfg.domain;
-in
-  {
+in {
   services.silverbullet = {
     user = username;
     group = "users";
-  
+
     inherit listenPort spaceDir;
 
     envFile = config.sops.secrets.silverbullet-env.path;
+
+    package = pkgs.silverbullet.overrideAttrs (oldAttrs: rec {
+      version = "0.10.4";
+      src = pkgs.fetchurl {
+        url = "https://github.com/silverbulletmd/silverbullet/releases/download/${version}/silverbullet.js";
+        hash = "sha256-ko1zXfvn0rVY+lp9zTZ71BL41h7AOazooBVeqELP3Ps=";
+      };
+    });
+  };
+
+  systemd.services = lib.mkIf config.services.silverbullet.enable {
+    silverbullet.serviceConfig = {
+      Environment = "DENO_DIR=/var/cache/silverbullet/deno";
+      CacheDirectory = "silverbullet";
+    };
   };
 
   services.caddy.virtualHosts = lib.mkIf config.services.silverbullet.enable {
@@ -25,9 +44,9 @@ in
     };
   };
 
-  # systemd.tmpfiles.rules = lib.mkIf config.services.silverbullet.enable [ 
+  # systemd.tmpfiles.rules = lib.mkIf config.services.silverbullet.enable [
   #   "d ${cfg.ssdPath}/silverbullet 0775 silverbullet users -"
-  #   "d ${spaceDir} 0775 silverbullet users -" 
-  #   "Z ${cfg.configPath} 0775 silverbullet users -" 
+  #   "d ${spaceDir} 0775 silverbullet users -"
+  #   "Z ${cfg.configPath} 0775 silverbullet users -"
   # ];
 }
