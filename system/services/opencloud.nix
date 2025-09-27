d374a62caf3f73fe6ac603c46231ee1cb4f73fc0
evolve <elivanoa5@gmail.com>
2025-09-27 16:46:02 +0200

    flop collabora

--- system/services/opencloud.nix
+++ system/services/opencloud.nix
@@ -1,56 +1,88 @@
-{
-  lib,
-  config,
-  ...
-}: let
-  cfg = config.server;
-  fqdn = cfg.domain;
+{ lib, config, ... }:
 
+let
+  fqdn = config.server.domain;
   cloud = {
     url = "https://cloud.${fqdn}";
     port = 9200;
   };
   collabora = {
-    port = 9980;
     url = "https://collabora.${fqdn}";
+    port = 9980;
   };
 in {
   services.opencloud = {
+    enable = true;
     inherit (cloud) url port;
     stateDir = "${config.server.dataPath}/opencloud";
-    # address = "0.0.0.0";
     user = "opencloud";
-    group = cfg.serverGroupName;
+    group = config.server.serverGroupName;
+
+    environmentFile = "/services-config/opencloud-secrets.yml";
+
     environment = {
       OC_INSECURE = "true";
       PROXY_TLS = "false";
-      OC_USERNAME = "admin";
-      OC_PASSWORD = "admin";
-      # IDM_ADMIN_PASSWORD = "admin";
-      # ADMIN_PASSWORD = "admin";
+      COLLABORATION_APP_NAME = "CollaboraOnline";
+      COLLABORATION_APP_PRODUCT = "Collabora";
+      COLLABORATION_APP_ADDR = collabora.url;
+      COLLABORATION_APP_ICON = "${collabora.url}/favicon.ico";
+      COLLABORATION_APP_INSECURE = "true";
+      COLLABORATION_WOPI_SRC = collabora.url;
+      OC_URL = cloud.url;
     };
-    environmentFile = "/services-config/opencloud.yml";
+
+    settings = {
+      app-registry = {
+        apps = [
+          {
+            name = "Collabora";
+            description = "Collabora Online";
+            route = "/apps/collabora";
+            wopi_url = collabora.url;
+            open_mode = "external";
+          }
+        ];
+      };
+      web = {
+        web = {
+          config = {
+            office = {
+              enable = true;
+              wopi_url = collabora.url;
+            };
+          };
+        };
+      };
+    };
   };
 
   services.collabora-online = {
-    inherit (collabora) port;
-    enable = true;
-    # inherit (config.services.opencloud) enable;
-
+    enable = false;
+    port = collabora.port;
     settings = {
-      storage.wopi."@allow" = true;
-      # ssl = {
-      #   enable = false;
-      #   termination = false;
-      # };
+      storage = {
+        wopi = {
+          "@allow" = true;
+        };
+      };
+      net = {
+        frame_ancestors = "cloud.${fqdn}";
+        post_allow = {
+          host = [ "127\\.0\\.0\\.1" "::1" "cloud.${fqdn}" ];
+        };
+      };
+      ssl = {
+        enable = false;
+      };
     };
-
     aliasGroups = [
       {
         host = cloud.url;
-        aliases = [ collabora.url ];
+        aliases = [ "cloud.${fqdn}" ];
       }
     ];
+    extraArgs = [ "--o:ssl.enable=false" ];
   };
 
   services.caddy.virtualHosts = lib.mkIf config.services.opencloud.enable {
@@ -58,13 +90,19 @@ in {
       extraConfig = ''
         import cfdns
         reverse_proxy http://localhost:${toString cloud.port}
+        encode gzip
       '';
     };
 
-    "${collabora.url}" = {
+    "${collabora.url}" = lib.mkIf config.services.collabora-online.enable {
       extraConfig = ''
         import cfdns
-        reverse_proxy http://localhost:${toString collabora.port}
+        reverse_proxy http://localhost:${toString collabora.port} {
+          header_up Host {host}
+          header_up X-Real-IP {remote}
+          header_up X-Forwarded-For {remote}
+          header_up X-Forwarded-Proto {scheme}
+        }
       '';
     };
   };
